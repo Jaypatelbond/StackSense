d2a62800aad4360302275cab7e4567ae1c21e546
Jay Patel <jaypatelbond7@gmail.com>
2026-05-09 09:30:00 +0530

    feat(collection): Create CollectionsViewModel to store categories maps

---
+++ app/src/main/java/com/stacksense/ui/screens/collections/CollectionsViewModel.kt
@@ -0,0 +1,6 @@
+package com.stacksense.ui.screens.collections
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+@HiltViewModel
+class CollectionsViewModel @Inject constructor() : ViewModel()
